b985b3e18e5792d103d247cae9e94341f9beda09
Crypte <louisconnect@pm.me>
2024-04-17 17:52:19 +0200

    tryp to find api route

--- components/Toolcard.tsx
+++ components/Toolcard.tsx
@@ -27,73 +27,12 @@ interface Tool {
   link: string;
 }
 
-async function GetTools() {
-  const res = await fetch('http://localhost:3000/api/tools');
-  const data = await res.json();
-  return data.TOOL_LIST;
-}
-
 async function Toolcard({ id }: ToolcardProps) {
-  const toolist = await GetTools();
-  const Tool = toolist.find((item: Tool) => item.id === id);
   
   return (
     <Suspense fallback={<SkeletonCard/>}>
     <Card>
-    <div className="relative rounded-2xl mb-4 lg:m-0 group transition-all">
-      <Link href={Tool.link} target="_blank">
-          <div className="p-5 gap-5 flex flex-col">
-            <div className="relative">
-            <AspectRatio ratio={16 / 9}>
-              <Image placeholder = 'blur' blurDataURL="https://placehold.co/1920x1080" className="rounded-lg object-cover" src={Tool.url_img} alt="logo" fill/>
-              </AspectRatio>
-              <div className="flex w-fit gap-1.5 flex-wrap absolute bottom-0 left-0 right-0 p-4">
-                {Tool.badge &&
-                  Tool.badge.map((badge:any,index:any) => (
-                    <Badge variant={badge} key={index} />
-                  ))}
-              </div>
-            </div>
-        
-            <div className="flex lg:ml-4">
-              <div className="mr-5">
-                <Image
-                  className="rounded-2xl"
-                  src={Tool.url_logo}
-                  alt={Tool.nom}
-                  width={64}
-                  height={64}
-                />
-              </div>
-              <div className="h-18 flex flex-col justify-between">
-              <h1 className="transition group-hover:underline text-2xl font-bold flex gap-2 items-center">
-                {Tool.nom}
-                <ExternalLink  size={20} className="transition lg:opacity-0 lg:group-hover:opacity-100" />
-              </h1>
-            
-              {Tool.platform && (
-              <div className="pointer-events-none font-bold p-1 text-main flex gap-3 items-center border-2 rounded-full w-fit ">
-                {Tool.platform.map((platform:any, index:any) => (
-                  <Platform variant={platform} key={index} />
-                ))}
-              </div>
-            )}
-            </div>
-            </div>
-           
-              <p className="text-base text-muted-foreground text-justify">{Tool.description}</p>
-        
-
-            {Tool.offer && (
-              <p
-                className="justify-center text-center rounded-md transition p-2 text-sm border border-primary text-foreground flex gap-2 items-center font-semibold">
-                Parrainage : {''}
-                {Tool.offer}
-              </p>
-            )}
-          </div>
-      </Link>
-    </div>
+    salut
     </Card>
     </Suspense>
     

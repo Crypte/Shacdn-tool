edfb546a073938d144cb6164ac76a8189eb779a4
Crypte <louisconnect@pm.me>
2024-04-17 17:04:15 +0200

    New deployment

--- components/Toolcard.tsx
+++ components/Toolcard.tsx
@@ -27,8 +27,13 @@ interface Tool {
   link: string;
 }
 
+const dev = process.env.NODE_ENV !== "production";
+const server = dev
+? "http://localhost:3000"
+: "https://shacdn-tool-git-i18n-feature-gitcryptes-projects.vercel.app";
+
 async function GetTools() {
-  const res = await fetch('https://shacdn-tool-git-i18n-feature-gitcryptes-projects.vercel.app/api/tools');
+  const res = await fetch(`${server}/api/tools`);
   const data = await res.json();
   return data.TOOL_LIST;
 }

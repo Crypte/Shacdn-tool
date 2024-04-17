3e879ac5c8c1efc4a808733af4509b65a91a341b
Crypte <louisconnect@pm.me>
2024-04-18 00:54:43 +0200

    Last test for today

--- components/Toolcard.tsx
+++ components/Toolcard.tsx
@@ -17,7 +17,7 @@ type ToolcardProps = {
 };
 
 async function getToolData(id:number) {
-  const res = await fetch(process.env.VERCEL_URL+`/api/tools/"${id}`);
+  const res = await fetch(`${BASE_API_URL}/api/tools/${id}`);
   try {
     const data = await res.json();
     return data.tool;

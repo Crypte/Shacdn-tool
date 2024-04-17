418fc4f88650ce36869435cf05bd2cf7bfe3ffbb
Crypte <louisconnect@pm.me>
2024-04-17 23:43:57 +0200

    try this

--- app/api/tools/[id]/route.ts
+++ app/api/tools/[id]/route.ts
@@ -1,14 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 import { TOOL_LIST } from "@/constants/tool_en_test";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { id } = req.query;
-  const tool = TOOL_LIST.find((item) => id === item.id.toString());
+export async function GET(request: Request, context: any) {
+  const { params } = context;
+  const tool = TOOL_LIST.find((item) => params.id === item.id.toString());
 
-  if (!tool) {
-    // Gérer le cas où aucun outil ne correspond à l'ID fourni
-    return res.status(404).json({ message: "Outil non trouvé" });
-  }
-
-  return res.status(200).json(tool);
+  return NextResponse.json(tool);
 }
--- app/api/tools/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import { TOOL_LIST } from "@/constants/tool_en_test";
-
-export async function GET() {
-    return NextResponse.json({
-        TOOL_LIST
-    }
-    )
-}

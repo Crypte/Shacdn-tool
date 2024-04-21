a1e7d2b9f7afbd09011b297223755102c6923271
Crypte <louisconnect@pm.me>
2024-04-21 22:26:23 +0200

    reduce image size for render speed

--- components/Toolcard.tsx
+++ components/Toolcard.tsx
@@ -59,6 +59,11 @@ function Toolcard({ id }: ToolcardProps) {
                     className="rounded-lg object-cover"
                     src={toolData.url_img}
                     alt="logo"
+                    sizes="(max-width: 300px) 100vw,
+                    (max-width: 500px) 100vw,
+                    (max-width: 800px) 100vw,
+                    (max-width: 1200px) 100vw,
+                    100vw"
                     fill
                   />
                 </AspectRatio>

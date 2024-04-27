d8994beaf0a326be71634ff6ddc6f5ee345a912d
Crypte <louisconnect@pm.me>
2024-04-27 19:34:15 +0200

    new config for img whitelist

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          { protocol: 'https', hostname: 'tool-image.s3.eu-north-1.amazonaws.com' }
-        ],
-      },
+  images: {
+    domains: ['tool-image.s3.eu-north-1.amazonaws.com'],
+},
 };
 
 export default nextConfig;
